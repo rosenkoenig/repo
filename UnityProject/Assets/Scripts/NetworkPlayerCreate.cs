801e4603ef62ad34d636e7286524e0c58547b108
Paul Vaille <paul.vaille@leikir-studio.fr>
2016-02-21 17:34:32 +0100

    caca osef

--- UnityProject/Assets/Scripts/NetworkPlayerCreate.cs
+++ UnityProject/Assets/Scripts/NetworkPlayerCreate.cs
@@ -7,7 +7,7 @@ public class NetworkPlayerCreate : NetworkBehaviour
     //Use this for initialization
     void Start ()
     {
-        SetOwner();
+        
     }
 
     void SetOwner()
@@ -19,14 +19,19 @@ public class NetworkPlayerCreate : NetworkBehaviour
             GetComponent<PlayerHandHUD>().cardParent = GameObject.Find("Player_1_HandHUD").transform;
             GameMaster.Instance.playerHandHuds[1] = GetComponent<PlayerHandHUD>();
         }
+        else
+        {
+            GetComponent<PlayerHandHUD>().owner = Owner.PLAYER_0;
+            GetComponent<Character>().owner = Owner.PLAYER_0;
+            GetComponent<PlayerHandHUD>().cardParent = GameObject.Find("Player_0_HandHUD").transform;
+            GameMaster.Instance.playerHandHuds[0] = GetComponent<PlayerHandHUD>();
+        }
     }
 
     public override void OnStartLocalPlayer()
     {
-        GetComponent<PlayerHandHUD>().owner = Owner.PLAYER_0;
-        GetComponent<Character>().owner = Owner.PLAYER_0;
-        GetComponent<PlayerHandHUD>().cardParent = GameObject.Find("Player_0_HandHUD").transform;
-        GameMaster.Instance.playerHandHuds[0] = GetComponent<PlayerHandHUD>();
+        SetOwner();
+
         GameMaster.Instance.StartGame();
     }
 }
